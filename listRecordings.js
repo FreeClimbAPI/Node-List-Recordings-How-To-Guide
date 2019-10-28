319b6a218952267217bd9522b4d16d35ce04e802
Francis San Filippo <fsanfilipp@vailsys.com>
2019-10-28 11:03:17 -0500

    freeclimb code changes

--- listRecordings.js
+++ listRecordings.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const persephonySDK = require('@persephony/sdk')
+const freeclimbSDK = require('@freeclimb/sdk')
 
 const accountId = process.env.ACCOUNT_ID
 const authToken = process.env.AUTH_TOKEN
-// your Persephony API key (available in the Dashboard) - be sure to set up environment variables to store these values
-const persephony = persephonySDK(accountId, authToken)
+// your freeclimb API key (available in the Dashboard) - be sure to set up environment variables to store these values
+const freeclimb = freeclimbSDK(accountId, authToken)
 
 getRecordings().then(recordings => {
   // Use recordings
@@ -16,13 +16,13 @@ async function getRecordings() {
   // Create array to store all recordings
   const recordings = []
   // Invoke GET method to retrieve initial list of recordings information
-  const first = await persephony.api.recordings.getList()
+  const first = await freeclimb.api.recordings.getList()
   recordings.push(...first.recordings)
   // Get Uri for next page
   let nextPageUri = first.nextPageUri
   // Retrieve entire recordings list 
   while (nextPageUri) {
-    const nextPage = await persephony.api.recordings.getNextPage(nextPageUri)
+    const nextPage = await freeclimb.api.recordings.getNextPage(nextPageUri)
     recordings.push(...nextPage.recordings)
     nextPageUri = nextPage.nextPageUri
   }
